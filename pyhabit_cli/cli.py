b575e7ab66cd822d421c875e7ae83e9e9312a693
Akshay Bist <elssar@marvin.(none)>
2013-05-03 07:06:58 +0530

    pep8

--- pyhabit_cli/cli.py
+++ pyhabit_cli/cli.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 
-from pprint import pprint
-
 import os
+from pprint import pprint
 import sys
 
 from pyhabit.api import HabitAPI
@@ -10,10 +9,11 @@ from pyhabit.api import HabitAPI
 USER_ID=os.environ["HABIT_USER_ID"]
 API_KEY=os.environ["HABIT_API_KEY"]
 
+
 def main():
     habitAPI = HabitAPI(USER_ID, API_KEY)
     func = getattr(habitAPI, sys.argv[1])
-    if callable( func ):
+    if callable(func):
         pprint(func(*sys.argv[2:]))
     else:
         print "Not a HabitRPG API method"
